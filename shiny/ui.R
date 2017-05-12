85e0a302dcabeb8081a6c9bad63fc8e1764b38c4
turkSLO <bhturk@calpoly.edu>
2017-05-11 21:38:57 -0700

    Clean code + Purpose and Limits + Blurb

--- shiny/ui.R
+++ shiny/ui.R
@@ -95,7 +95,21 @@ shinyUI(bootstrapPage(
         });
         if(localStorage.firstTime !== 'false') {
            $('.overlay, .overlay-back').each(function(){$(this).toggle();});
-           firstTime.next($('.col-sm-4'), 'Please insert:</br><input type=\"text\" style=\"width:575px; color:black;\" value=\"https://en.wikipedia.org/wiki/List_of_current_United_States_Senators\" /></br>into the bar on the left and press Submit.', 'right');
+           firstTime.next($('.col-sm-4'), 'Please insert:</br>' +
+            '<input type=\"text\" style=\"width:575px; color:black;\" value=\"https://en.wikipedia.org/wiki/List_of_current_United_States_Senators\" /></br>' +
+            'into the bar on the left and press Submit.</br></br></br></br>'+
+            '<center><h3>Purpose and Limitations</h3></center></br>'+
+            'There are data tables online everywhere that lack simple ways to retrieve them.</br>' +
+            'This Shiny App was designed for the purpose of taking those tables and ripping them into a file that is commonly used for data management programs and statistical software.' +
+            'However, even this app has its shortcomings.' +
+            'This app cannot clean the data for you, it just gives you what it sees on the webpage and does its best to create a properly formatted data file.' +
+            'So you should always check the data table you download carefully for any weirdness that might have been hidden away by the web designer.</br>' +
+            'There are other limitations of this app and they are:</br>' +
+            '<ul>'+
+            '<li>The table cannot be something you have to search up to have appear. (Tables must be static on the page.)</li>' +
+            '<li>Web pages must be accessible to anyone. (There cannot be a login page that the tables are hidden behind.)</li>' +
+            '<li>The tables must use standard HTML formatting. (Some web designers makes things that are not tables, look like tables.)</li>' +
+            '</ul>', 'right');
         }
         $('#exit-guide').click(function(){
           $('.overlay, .overlay-back').each(function(){$(this).toggle();});
@@ -105,15 +119,36 @@ shinyUI(bootstrapPage(
         $('#enter-guide').click(function(){
           $('.overlay, .overlay-back').each(function(){$(this).toggle();});
           localStorage.firstTime = true;
-          firstTime.next($('.col-sm-4'), 'Please insert:</br><input type=\"text\" style=\"width:575px; color:black;\" value=\"https://en.wikipedia.org/wiki/List_of_current_United_States_Senators\" /></br>into the bar on the left and press Submit.', 'right');
+          firstTime.next($('.col-sm-4'), 'Please insert:</br>' +
+            '<input type=\"text\" style=\"width:575px; color:black;\" value=\"https://en.wikipedia.org/wiki/List_of_current_United_States_Senators\" /></br>' +
+            'into the bar on the left and press Submit.</br></br></br></br>'+
+            '<center><h3>Purpose and Limitations</h3></center></br>'+
+            'There are data tables online everywhere that lack simple ways to retrieve them.</br>' +
+            'This Shiny App was designed for the purpose of taking those tables and ripping them into a file that is commonly used for data management programs and statistical software.' +
+            'However, even this app has its shortcomings.' +
+            'This app cannot clean the data for you, it just gives you what it sees on the webpage and does its best to create a properly formatted data file.' +
+            'So you should always check the data table you download carefully for any weirdness that might have been hidden away by the web designer.</br>' +
+            'There are other limitations of this app and they are:</br>' +
+            '<ul>'+
+            '<li>The table cannot be something you have to search up to have appear. (Tables must be static on the page.)</li>' +
+            '<li>Web pages must be accessible to anyone. (There cannot be a login page that the tables are hidden behind.)</li>' +
+            '<li>The tables must use standard HTML formatting. (Some web designers makes things that are not tables, look like tables.)</li>' +
+            '</ul>', 'right');
         });
         $('#submitURL').click(function(){
-          if(localStorage.firstTime !== 'false') {
-            firstTime.next($('.col-sm-8'), 'On the right will populate the tables located within that page.</br><button id=\"continue\" class=\"btn btn-default\" type=\"button\">Continue</button>', 'left');
+          if(localStorage.firstTime !== 'false' && /https:\\/\\/en\\.wikipedia\\.org\\/wiki\\/List_of_current_United_States_Senators/.test($('#url').val())) {
+            firstTime.next($('.col-sm-8'), 'On the right will populate the tables located within that page.</br>' +
+              '<button id=\"continue\" class=\"btn btn-default\" type=\"button\">Continue</button>', 'left');
           }
         });
         $('#guide-content').on('click','#continue',function(){
-         firstTime.next($('.col-sm-4'), 'Using the arrow buttons above, you can preview the tables within the page.</br>With the empty bar below, you can select which tables you wish to download.</br>After selecting one or more tables, more buttons appear:</br>You can choose to invert the selection (pick the opposite of what you had selected).</br>You can choose to download the tables in CSV format.</br>You can choose to download the tables in TXT format.</br></br>This is the end of the tutorial. To close, hit the button on the bottom right.', 'bottom');
+         firstTime.next($('.col-sm-4'), 'Using the arrow buttons above, you can preview the tables within the page.</br>' +
+          'With the empty bar below, you can select which tables you wish to download.</br>' +
+          'After selecting one or more tables, more buttons appear:</br>' +
+          'You can choose to invert the selection (pick the opposite of what you had selected).</br>' +
+          'You can choose to download the tables in CSV format.</br>' +
+          'You can choose to download the tables in TXT format.</br></br>' +
+          'This is the end of the tutorial. To close, hit the button on the bottom right.', 'bottom');
         $('.col-sm-8').addClass('guided');
          });
         $('#guide-content').on('click','input:text',function(){
@@ -136,7 +171,7 @@ shinyUI(bootstrapPage(
             pos = el.offset();
             if (loc == 'bottom') {
               left = pos.left > 0 ? pos.left : 10;
-              top = pos.top + el.outerHeight();
+              top = pos.top + el.outerHeight()+30;
             } else if (loc == 'right') {
               top = pos.top;
               left = pos.left + el.outerWidth();
